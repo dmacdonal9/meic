fd6bf9c13695d5344cfbbaa7cfe73b87e260101c
darcymac <darcymac@gmail.com>
2024-11-03 18:35:09 -0400

    initial

--- cfg.py
+++ cfg.py
@@ -4,3 +4,7 @@ short_put_offset = 0
 long_put_width = 75
 short_call_offset = 0
 long_call_width = 75
+
+ib_host='127.0.0.1'
+ib_port='7497'
+ib_clientid='1'
--- ib_instance.py
+++ ib_instance.py
@@ -1,5 +1,28 @@
+import logging
 from ib_insync import IB
+import time
 
-# Initialize and connect IB instance
+import cfg
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Initialize IB instance
 ib = IB()
-ib.connect('127.0.0.1', 7497, clientId=1)
+
+# Variables for retry mechanism
+max_retries = 5  # Maximum number of retries if the connection fails
+retry_interval = 2  # Time (in seconds) to wait between retries
+
+# Try to connect
+for attempt in range(max_retries):
+    try:
+        logger.info(f"Attempt {attempt + 1} to connect to Interactive Brokers...")
+        ib.connect(cfg.ib_host,cfg.ib_port,cfg.ib_clientid)
+        logger.info('Successfully connected to Interactive Brokers!')
+        break
+    except Exception as e:
+        logger.error(f'Failed to connect to Interactive Brokers on attempt {attempt + 1}. Error: {str(e)}')
+        if attempt < max_retries - 1:  # No need to sleep on the last attempt
+            time.sleep(retry_interval)
